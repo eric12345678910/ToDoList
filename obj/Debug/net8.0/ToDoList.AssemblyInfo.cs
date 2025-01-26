2160f8f573da9898329cb6db5ecd817a31b1b4ea
eric12345678910 <ericlaudrum@gmail.com>
2025-01-26 18:25:27 -0500

    Add Create.cshtml to handle creation of new lists

--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc879b60a0cc8ae217c3f55e6ce9ef38061d5045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc1ad2343358a5018bdc16c1db7aab131652ad1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
