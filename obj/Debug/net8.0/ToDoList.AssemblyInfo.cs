0f0b6a96c49273203da38af9e583c52e6cd52229
eric12345678910 <ericlaudrum@gmail.com>
2025-02-02 12:00:12 -0500

    Add option to add new item on Task List index page

--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef64fb45264c21d0c646f6da9278094f4a74e8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91933251e9af65420b40c15b70ae71cff00c7b86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
