4d965402c208dfd481a64c6629f6892f5c3e42f9
eric12345678910 <ericlaudrum@gmail.com>
2025-02-02 13:24:38 -0500

    Change redirect from adding new items to the same page

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -8,13 +8,13 @@ using Microsoft.AspNetCore.Mvc;
 public class ProjectController : Controller
 {
     private static List<TaskList> _taskLists = new List<TaskList>();
-    
+
     [HttpGet]
     public IActionResult Index()
     {
         return View(_taskLists); // Output all task lists
     }
-    
+
     // Form to create a new list
     [HttpGet]
     public IActionResult Create()
@@ -32,9 +32,10 @@ public class ProjectController : Controller
             _taskLists.Add(taskList);
             return RedirectToAction("Index");
         }
+
         return View(taskList); // Redirect back if model is not valid
     }
-    
+
     [HttpGet]
     public IActionResult TaskList(int listId)
     {
@@ -43,11 +44,24 @@ public class ProjectController : Controller
         {
             return NotFound();
         }
+
         // //////////////////////////////////////// Debugging 
         Console.WriteLine($"List Found: {list.Title}");
         return View(list);
     }
- 
+
+/*
+    [HttpGet]
+    public IActionResult AddItem(int listId)
+    {
+        var list = _taskLists.FirstOrDefault(l => l.ListId == listId);
+        if (list == null)
+        {
+            return NotFound();
+        }
+        return View(list);
+    }
+
 
     [HttpGet]
     public IActionResult AddItem(int listId)
@@ -60,26 +74,30 @@ public class ProjectController : Controller
         return View(list);
     }
 
+*/
     [HttpPost]
     [ValidateAntiForgeryToken]
     public IActionResult AddItem(int listId, string itemName)
     {
         var list = _taskLists.FirstOrDefault(l => l.ListId == listId);
+        // Handle errors: empty list
         if (list == null)
         {
             return NotFound();
         }
 
+        // Handle errors: empty input string
         if (string.IsNullOrWhiteSpace(itemName))
         {
             ModelState.AddModelError("", "Item name cannot be empty.");
             return View(list);
         }
-        
-        
+
+        // Add item to current list
         list.Items.Add(new TaskItem(itemName));
 
-        return RedirectToAction("TaskList", new{listId});
+        return RedirectToAction("index");
     }
+
 }
 
--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91933251e9af65420b40c15b70ae71cff00c7b86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0b6a96c49273203da38af9e583c52e6cd52229")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
